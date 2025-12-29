099971413fddd0e36dfad90faeb473695864adc8
Dell <marella.srinivasa020@gmail.com>
2025-12-29 23:57:08 +0530

    as

--- src/models/booking.js
+++ src/models/booking.js
@@ -2,7 +2,7 @@ export default (sequelize, DataTypes) => {
   const Booking = sequelize.define(
     "Booking",
     {
-      booking_id: {
+      id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,

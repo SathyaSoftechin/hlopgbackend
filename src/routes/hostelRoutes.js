1780d9f8c46cad28f87012f23f811b9034a7a4e4
Dell <marella.srinivasa020@gmail.com>
2026-01-06 00:46:12 +0530

    As

--- src/routes/hostelRoutes.js
+++ src/routes/hostelRoutes.js
@@ -3,7 +3,7 @@ import multer from "multer";
 import { verifyOwnerToken, authenticateUserToken } from "../middleware/authMiddleware.js";
 
 
-import {  getHostels, hydHostels, cheHostels, benHostels, getHostelById, addHostel, updateHostel, toggleLikeHostel, getLikedHostels   } from "../controllers/hostelController.js";
+import {  getHostels, hydHostels, cheHostels, benHostels, getHostelById, addHostel, updateHostel,   } from "../controllers/hostelController.js";
 
 const router = express.Router();
 const upload = multer();
